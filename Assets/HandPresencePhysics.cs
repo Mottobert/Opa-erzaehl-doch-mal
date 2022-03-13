8201e3c1b4d987ea845bba20a23193ff59682e68
Max Otto <62698292+Mottobert@users.noreply.github.com>
2022-03-13 20:15:50 +0100

    Added Teleportation + Physics Hands Presence
    Addded Teleportation in Forest Scene, Added Physics Hands Presence in Flood Scene

---
+++ Assets/HandPresencePhysics.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPresencePhysics : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rb;
+    public Renderer nonPhysicalHand;
+    public float showNonPhysicalHandDistance = 0.05f;
+    private Collider[] handColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void EnableHandCollider()
+    {
+       foreach(var item in handColliders)
+       {
+           item.enabled = true;
+       }
+    }
+
+    public void EnableHandColliderDelay(float delay)
+    {
+        Invoke("EnableHandCollider", delay);
+    }
+
+    public void DisableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if(distance > showNonPhysicalHandDistance)
+        {
+            nonPhysicalHand.enabled = true;
+        }
+        else
+        {
+            nonPhysicalHand.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // position
+        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+
+        // rotation
+        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+
+        Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
+
+        rb.angularVelocity = (rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
+    }
+}
