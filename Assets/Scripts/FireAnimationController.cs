22489ee47ae8eb2d61bb3fa42fa5043218bbf496
Max Otto <62698292+Mottobert@users.noreply.github.com>
2022-03-30 16:07:22 +0200

    Forest Scene
    Falling Ash, adjusted Flames on Ignis Trees

--- Assets/Scripts/FireAnimationController.cs
+++ Assets/Scripts/FireAnimationController.cs
@@ -7,11 +7,24 @@ public class FireAnimationController : MonoBehaviour
     [SerializeField]
     private GameObject[] particleSystems;
 
+    [SerializeField]
+    private float minDelay;
+    [SerializeField]
+    private float maxDelay;
+
+
     public void ActivateParticleSystems()
     {
-        foreach(GameObject ps in particleSystems)
+        //ps.GetComponentInChildren<ParticleSystem>().Play();
+        StartCoroutine(ActivateFireDelay(minDelay, maxDelay));
+    }
+
+    IEnumerator ActivateFireDelay(float minDelay, float maxDelay)
+    {
+        foreach (GameObject ps in particleSystems)
         {
             ps.GetComponentInChildren<ParticleSystem>().Play();
+            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
         }
-    } 
+    }
 }
